5d3d2e02eff5954ed15eb074255b206bd5caa90a
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-05-29 11:22:08 -0600

    implementando interfaz

--- src/Proveedores.java
+++ src/Proveedores.java
@@ -1,9 +1,23 @@
-public class Proveedores {
+import java.util.ArrayList;
+
+public class Proveedores implements IFileLectura {
     private String nombre;
     private int telefono;
     private String email;
     private String descripcion;
     public Proveedores() {
+    }
+
+    private ArrayList<Proveedores> proveeedoresEmigrados = new ArrayList<Proveedores>();
+
+    private int indexNombre = -1;
+    private int indexTelefono = -1;
+    private int indexDescripcion = -1;
+    private int indexEmail = -1;
+
+    
+    public ArrayList<Proveedores> getProveeedoresEmigrados() {
+        return proveeedoresEmigrados;
     }
     public Proveedores(String nombre, int telefono, String email, String descripcion) {
         this.nombre = nombre;
@@ -35,5 +49,56 @@ public class Proveedores {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+    private void asignarIndices(String linea) {
+        String[] splitNameCampo = linea.split(",");
+        for (int i = 0; i < splitNameCampo.length; i++) {
+            String campo = splitNameCampo[i];
+            switch (campo) {
+                case "Nombre":
+                    indexNombre = i;
+                    break;
+                case "Teléfono":
+                    indexTelefono = i;
+                    break;
+                case "Email":
+                    indexEmail = i;
+                    break;
+                    case "Descripción":
+                    indexDescripcion = i;
+                    break;
+            }
+        }
+    }
+
+
+    @Override
+    public void leerLinea(String linea, int numLinea) {
+        if (numLinea == 1) {
+            asignarIndices(linea);
+        } else {
+            Proveedores proveedores = new Proveedores();
+            proveeedoresEmigrados.add(proveedores);
+            String[] splitValueCampo = linea.split(",");
+            for (int i = 0; i < splitValueCampo.length; i++) {
+                String valor = splitValueCampo[i].trim();
+                if (i == indexNombre) {
+                    proveedores.setNombre(valor);
+                }
+                else if (i == indexTelefono) {
+                    proveedores.setTelefono(Integer.parseInt(valor));
+                }
+                else if (i == indexEmail) {
+                    proveedores.setEmail(valor);
+                }
+                else if (i == indexDescripcion) {
+                    proveedores.setEmail(valor);
+                }
+                
+            }
+        }
+    }
+        
+    }
     
-}
+
